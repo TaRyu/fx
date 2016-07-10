c61cedd952f410c1e0cdbcbd8b1fbf5af0adf69a
TaRyu <taryu1991@gmail.com>
2016-07-10 20:50:03 +0800

    compere test

--- compere_re.py
+++ compere_re.py
@@ -6,7 +6,6 @@ from __future__ import print_function
 
 import numpy as np
 import pandas as pd
-import time
 
 from sklearn.svm import SVR
 from sklearn.neighbors import KNeighborsRegressor
@@ -20,14 +19,8 @@ res = [SVR(kernel='linear'), SVR(),
        KNeighborsRegressor(), RandomForestRegressor()]
 time_format = '%Y%m%d%H%M'
 result_tmp = np.empty(0)
+num_test = 1106
 
-
-def columns_define():
-    columns = []
-    for name in names:
-        columns.append('%s(S)' % name)
-        columns.append('%s(T)' % name)
-    return columns
 
 if __name__ == '__main__':
     for fx in FX_LIST:
@@ -38,20 +31,19 @@ if __name__ == '__main__':
         range_price = data_s['max_price'] - data_s['min_price']
         data = np.array([(data[i] - data_s['min_price'][i]) /
                          range_price[i] for i in range(data.shape[0])])
-        data_train = data[:data.shape[0] - 354]
-        data_test = data[data.shape[0] - 354:]
-        data_s_train = data_s[:data.shape[0] - 354]
-        data_s_test = data_s[data.shape[0] - 354:]
+        data_train = data[:data.shape[0] - num_test]
+        data_test = data[data.shape[0] - num_test:]
+        data_s_train = data_s[:data.shape[0] - num_test]
+        data_s_test = data_s[data.shape[0] - num_test:]
         for i in range(len(names)):
             re = res[i]
-            pass
-            start = time.time()
             re.fit(data_train, data_s_train['change'])
-            end = time.time()
-            time_cost = end - start
+            prediction = re.predict(data_test)
             score = metrics.explained_variance_score(
-                data_s_test['change'], re.predict(data_test))
-            result_tmp = np.append(result_tmp, [score, time_cost])
-    result = pd.DataFrame(result_tmp.reshape(-1, 2 * len(names)),
-                          index=FX_LIST, columns=columns_define())
+                data_s_test['change'], prediction)
+            result_tmp = np.append(result_tmp, score)
+            data_s_test['%s' % names[i]] = prediction
+        data_s_test.to_pickle('%s/%s_com_pre_re.pkl' % (FILE_PREX, fx))
+    result = pd.DataFrame(result_tmp.reshape(-1, len(names)),
+                          index=FX_LIST, columns=names)
     result.to_pickle('../data/fx/result_compere_re.pkl')
