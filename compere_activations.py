0366e71cd1dbe10dfff502664d35f8fee22f5a67
taryu <taryu1991@gmail.com>
2016-07-11 22:29:34 +0800

    605 test

--- compere_activations.py
+++ compere_activations.py
@@ -12,15 +12,15 @@ import time
 import tensorflow as tf
 from tensorflow.contrib import learn
 
-# FX_LIST = ['EURUSD', 'USDJPY', 'GBPUSD', 'AUDUSD', 'EURJPY']
+FX_LIST = ['EURUSD', 'USDJPY', 'GBPUSD', 'AUDUSD', 'EURJPY']
 # FILE_PREX = '../data/fx'
 # names = ['relu', 'relu6', 'elu', 'softplus', 'softsign', 'sigmoid', 'tanh']
 # activations = [tf.nn.relu, tf.nn.relu6, tf.nn.elu,
 #                tf.nn.softplus, tf.nn.softsign, tf.sigmoid, tf.tanh]
-FX_LIST = ['EURUSD']
+# FX_LIST = ['EURUSD']
 FILE_PREX = '../data/fx'
-names = ['relu']
-activations = [tf.nn.relu]
+names = ['relu', 'softplus']
+activations = [tf.nn.relu, tf.nn.softplus]
 
 
 def max_pool_2x2(tensor_in):
@@ -30,7 +30,7 @@ def max_pool_2x2(tensor_in):
 
 time_format = '%Y%m%d%H%M'
 result_tmp = np.empty(0)
-num_test = 856
+num_test = 605
 
 if __name__ == '__main__':
     for fx in FX_LIST:
