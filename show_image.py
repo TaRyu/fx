f55b829658120788db31a4e3b73c4cc11d1e2a1c
TaRyu <taryu1991@gmail.com>
2016-07-20 19:21:18 +0800

    test 20000 500

---
+++ show_image.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig, ax = plt.subplots()
+
+n = np.load('../data/fx/EURUSD_FINAL_M.npy')[0]
+
+n_range = np.max(n) - np.min(n)
+image = np.array([(n[i] - np.min(n)) / n_range for i in range(len(n))])
+ax.imshow(image.reshape(24, 24), cmap=plt.cm.gray, interpolation='nearest')
+# ax.set_title('dropped spines')
+
+# Move left and bottom spines outward by 10 points
+ax.spines['left'].set_position(('outward', 24))
+ax.spines['bottom'].set_position(('outward', 24))
+# Hide the right and top spines
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+# Only show ticks on the left and bottom spines
+ax.yaxis.set_ticks_position('left')
+ax.xaxis.set_ticks_position('bottom')
+
+font = {'family': 'serif',
+        'weight': 'normal',
+        'size': 20,
+        }
+
+plt.xlabel('24 hours', fontdict=font)
+plt.ylabel('24 business days', fontdict=font)
+
+plt.show()
