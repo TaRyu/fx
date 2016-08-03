b8ae7d89e2be15796d606c0ebc9ee9a1d508a3ec
taryu <taryu1991@gmail.com>
2016-08-03 15:47:53 +0800

    0.5 50

--- compere_op.py
+++ compere_op.py
@@ -42,7 +42,7 @@ def conv_model(X, y):
     # densely connected layer with 1024 neurons
     with tf.variable_scope('FC_Layer'):
         h_fc1 = learn.ops.dnn(
-            h_pool2_flat, [1024], activation=tf.nn.relu, dropout=0.2)
+            h_pool2_flat, [1024], activation=tf.nn.relu, dropout=0.5)
     with tf.variable_scope('LR_Layer'):
         o_linear = learn.models.linear_regression(h_fc1, y)
     return o_linear
@@ -60,14 +60,14 @@ if __name__ == '__main__':
                 re = learn.TensorFlowEstimator(
                     model_fn=conv_model,
                     n_classes=0,
-                    batch_size=100, steps=20000,
+                    batch_size=50, steps=20000,
                     optimizer=tf.train.MomentumOptimizer(
                         learning_rate=0.001, momentum=0.5))
             else:
                 re = learn.TensorFlowEstimator(
                     model_fn=conv_model,
                     n_classes=0,
-                    batch_size=100, steps=20000,
+                    batch_size=50, steps=20000,
                     optimizer=optimizer,
                     learning_rate=0.001)
             path_f_final = ['%s/%s_FINAL_M.npy' % (FILE_PREX, fx),
